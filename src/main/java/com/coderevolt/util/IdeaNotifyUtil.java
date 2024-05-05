d58de4582f019562e3cf7e3e2ff9073a99f08ca4
songbiao <646997146@qq.com>
2024-05-05 21:16:22 +0800

    fix(client): NotificationGroup版本兼容
    1. 2020.3版本后不支持java中创建NotificationGroup

--- src/main/java/com/coderevolt/util/IdeaNotifyUtil.java
+++ src/main/java/com/coderevolt/util/IdeaNotifyUtil.java
@@ -2,12 +2,11 @@ package com.coderevolt.util;
 
 import com.intellij.notification.*;
 
-import java.util.UUID;
-
 public class IdeaNotifyUtil {
 
     public static void notify(String content, NotificationType type) {
-        NotificationGroup notificationGroup = new NotificationGroup(UUID.randomUUID().toString().replace("-", ""), NotificationDisplayType.BALLOON, true);
+        NotificationGroupManager groupManager = NotificationGroupManager.getInstance();
+        NotificationGroup notificationGroup = groupManager.getNotificationGroup("notifyAction");
         Notification notification = notificationGroup.createNotification(content, type);
         Notifications.Bus.notify(notification);
     }
